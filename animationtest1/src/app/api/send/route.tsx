364f5634d1825a0d53d23a6ab62a60f9c5380059
Evan Yu <yuevan7@gmail.com>
2024-06-17 23:08:55 -0400

    reset to different api key

--- animationtest1/src/app/api/send/route.tsx
+++ animationtest1/src/app/api/send/route.tsx
@@ -2,7 +2,7 @@
 import { EmailTemplate } from '@/app/components/EmailTemplate';
 import { Resend } from 'resend';
 
-const resend = new Resend('re_cL1E9uyQ_KrUMMv6wzL4gYMxan8im1ZTm');
+const resend = new Resend(process.env.RESEND_API_KEY);
 const fromEmail= process.env.FROM_EMAIL;
 
 export async function POST(req: { body: any; }, res: any) {
