a7618a8894cf957e945915d72a545649c525c0d4
Evan Yu <yuevan7@gmail.com>
2024-06-18 00:08:45 -0400

    email routing issue

--- animationtest1/src/app/api/send/route.tsx
+++ animationtest1/src/app/api/send/route.tsx
@@ -1,34 +1,33 @@
-
+import { NextApiRequest, NextApiResponse } from 'next';
 import { EmailTemplate } from '@/app/components/EmailTemplate';
 import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
-const fromEmail= process.env.FROM_EMAIL;
+const fromEmail = process.env.FROM_EMAIL;
 
-export async function POST(req: { body: any; }, res: any) {
-  const {body} = req; 
-  const {email, subject, message} = body;
+export async function POST(req: NextApiRequest, res: NextApiResponse) {
+  const { email, subject, message } = req.body;
 
   try {
     const { data, error } = await resend.emails.send({
       from: fromEmail ?? 'default@example.com',
       to: ['yuevan7@gmail.com'],
-      subject: 'subject',
+      subject: subject,
       react: (
         <>
           <h1>{subject}</h1>
           <p>Thank you for contacting me!</p>
           <p>{message}</p>
         </>
-      ), 
+      ),
     });
 
     if (error) {
-      return Response.json({ error }, { status: 500 });
+      return res.status(500).json({ error });
     }
 
-    return Response.json(data);
+    return res.status(200).json(data);
   } catch (error) {
-    return Response.json({ error }, { status: 500 });
+    return res.status(500).json({ error });
   }
 }
