c15dfcba888cf96e9fc57453350fd792fa651112
Evan Yu <yuevan7@gmail.com>
2024-07-04 16:25:13 -0400

    Deployment for now (changing email method api)

--- animationtest1/src/app/page.tsx
+++ animationtest1/src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
           <HeroSection />
           <AboutSection />
           <Projects />
-          <EmailSection />
+          
         </div>
         <Footer />
     </main>
